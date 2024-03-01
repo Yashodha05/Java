c3e68f8ab928edc1594429d3ca5deeb12138b00f
Yashodha A <154966316+Yashodha05@users.noreply.github.com>
2024-03-01 09:19:59 -0800

    Add files via upload

---
+++ Diagnalofmatrices.java
@@ -0,0 +1,30 @@
+package Week7;
+import java.util.Scanner;
+public class Diagnalofmatrices {
+	public static void main(String[] args) {
+		int matrix[][]= new int[3][3];
+		Scanner inScanner= new Scanner(System.in);
+		System.out.println("enter matric elements one by one:");
+		for (int i=0; i<3; i++) {
+			for (int j=0; j<3; j++) {
+				matrix[i][j]= inScanner.nextInt();
+			
+			}
+		}
+		for (int i=0; i<3; i++) {
+			for (int j=0; j<3; j++) {
+				if ( i==j)
+				{				
+					System.out.println(matrix[i][j] + " ");
+				}
+				else {
+					System.out.println("*");
+				}
+				
+			}
+			System.out.println("\t");
+		}
+		inScanner.close();
+	}
+
+}
