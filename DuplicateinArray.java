4085b02d0b4df9d33ffd2183415258c27b43a7e1
Yashodha A <154966316+Yashodha05@users.noreply.github.com>
2024-03-04 08:16:35 -0800

    Add files via upload
    week7- P25. check whether duplicate element present in an array or not.

---
+++ DuplicateinArray.java
@@ -0,0 +1,29 @@
+package Week7;
+import java.util.Scanner;
+public class DuplicateinArray {
+	public static void main(String[] args) {
+		int array[] = new int[10]; boolean isDuplicate=false;
+		Scanner inScanner= new Scanner (System.in);
+		System.out.println("enter array size"); int size= inScanner.nextInt();
+		System.out.println("enter elements one by one");
+		for (int i=0; i<size; i++) 
+			array[i]= inScanner.nextInt();
+		int i,j=0;
+		outer:for (i =0; i<size-1; i++) 
+		{
+			for  (j= i+1; j<size; j++)
+			{
+				if (array[j]== array[i])
+				{
+					isDuplicate= true; break outer;
+				}
+			}
+		}	
+		
+		if (isDuplicate)
+			System.out.println("array conatains duplicate elements at position " + (i+1) + " and at " +  (j+1));
+		else
+			System.out.println("array doesn't contains duplicate elements ");
+	}
+
+}
